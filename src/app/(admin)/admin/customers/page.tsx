5307bdbfe42c560d484cc8d7b5cc119ea2dd17aa
AGL_Mạch Văn Hoàng (ホアン) <81142674+machvanhoang@users.noreply.github.com>
2023-10-13 17:33:38 +0700

    Customer

--- src/app/(admin)/admin/customers/page.tsx
+++ src/app/(admin)/admin/customers/page.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
 import Paginate from "@/AdminComponent/Paginate";
 import { SvgPlus, SvgEdit, SvgTrash } from "@/AdminComponent/Svg";
-export default function AdminCustomers() {
+import { fetchAdmin } from "@/lib/fetch";
+async function getCustomers() {
+  const res = await fetchAdmin("http://localhost:3000/customers.json");
+  return await res.json();
+}
+export default async function AdminCustomers() {
+  const customers = await getCustomers();
   return (
     <>
       <div className="d-flex justify-content-between mb-3">
         <h4 className="fw-bold">
           <span className="text-muted fw-light">Admin /</span> Customers
         </h4>
-        <Link href={`/admin/customers/create`} className="btn btn-primary btn-add-back">
+        <Link
+          href={`/admin/customers/create`}
+          className="btn btn-primary btn-add-back"
+        >
           <SvgPlus />
           Add customer
         </Link>
@@ -33,39 +42,50 @@ export default function AdminCustomers() {
                   </tr>
                 </thead>
                 <tbody className="table-border-bottom-0">
-                  <tr>
-                    <td>1</td>
-                    <td>
-                      <a
-                        className="strong-name"
-                        href="{{ route('admin.customer.edit', $customer) }}"
-                      >
-                        <strong>David Mach</strong>
-                      </a>
-                    </td>
-                    <td>username01</td>
-                    <td>hoangmach.website@gmail.com</td>
-                    <td>0969 874 264</td>
-                    <td>2023-10-10 10h00</td>
-                    <td>2023-10-10 10h00</td>
-                    <td>
-                      <span className="badge badge-published">Published</span>
-                    </td>
-                    <td>
-                      <div className="dropdown">
-                        <a className="btn btn-secondary btn-custom" href="">
-                          <span>
-                            <SvgEdit />
-                          </span>
-                        </a>
-                        <button className="btn btn-danger btn-custom ms-2 btnDelete">
-                          <span>
-                            <SvgTrash />
-                          </span>
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
+                  {customers &&
+                    customers.length &&
+                    customers.map((customer: any, key: number) => {
+                      return (
+                        <tr key={key}>
+                          <td>{key}</td>
+                          <td>
+                            <Link
+                              className="strong-name"
+                              href={`/admin/customers/${key}`}
+                            >
+                              <strong>{customer.full_name}</strong>
+                            </Link>
+                          </td>
+                          <td>{customer.username}</td>
+                          <td>{customer.email}</td>
+                          <td>{customer.phone}</td>
+                          <td>{new Date().toDateString()}</td>
+                          <td>{new Date().toDateString()}</td>
+                          <td>
+                            <span className="badge badge-published">
+                              Published
+                            </span>
+                          </td>
+                          <td>
+                            <div className="dropdown">
+                              <Link
+                                className="btn btn-secondary btn-custom"
+                                href={`/admin/customers/${key}`}
+                              >
+                                <span>
+                                  <SvgEdit />
+                                </span>
+                              </Link>
+                              <button className="btn btn-danger btn-custom ms-2 btnDelete">
+                                <span>
+                                  <SvgTrash />
+                                </span>
+                              </button>
+                            </div>
+                          </td>
+                        </tr>
+                      );
+                    })}
                 </tbody>
               </table>
             </div>
